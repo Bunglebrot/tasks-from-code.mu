e638ac1a9c5894186c02196b5918f9d60779fdfd
Nick Khitrov <th1sistom@yandex.ru>
2021-02-01 21:32:38 +0300

    Add a script of solutions for tasks of a Converting types

---
+++ 0 Основной учебник JavaScript/00 Переменные и операции/9 Принудительное преобразование типов/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Задача 9.1
+let a = '10';
+let b = '20';
+console.log(Number(a) + Number(b));
+
+// Задача 9.5
+a = '2';
+b = '3';
+console.log(+a + +b);
+
+// Задача 9.6
+let firstPx = '5px';
+let secondPx = '6px';
+console.log(parseInt(firstPx) + parseInt(secondPx));
+
+// Задача 9.7
+let firstFloatPx = '5.5px';
+let secondFloatPx = '6.25px';
+let sumFloatPx = parseFloat(firstFloatPx) + parseFloat(secondFloatPx);
+console.log(sumFloatPx);
+
+// Задача 9.8
+console.log(sumFloatPx + 'px');
+
+// Задача 9.9
+let num1 = 95;
+let num2 = 101;
+console.log(String(num1) + String(num2));
+
+// Задача 9.10 Поиск длины числа
+let num = 10950;
+console.log(String(num).length);
+
+// Задача 9.11
+console.log(String(num).length + String(num2).length);
