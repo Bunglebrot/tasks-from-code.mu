e849a0f792feba8fa3195b0e109f7bc8bcddf8a1
Nick Khitrov <th1sistom@yandex.ru>
2021-02-01 13:28:52 +0300

    Add a script of solutions for tasks of Rest primitives

---
+++ 0 Основной учебник JavaScript/00 Переменные и операции/5 Специальные значения/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Задача 5.1 undefined
+let variable;
+alert(variable);
+
+// Задача 5.2 null
+variable = null;
+alert(variable);
+
+// Задача 5.3 true
+let booleanTrue = true;
+alert(booleanTrue);
+
+// Задача 5.4 false
+let booleanFalse = false;
+alert(booleanFalse);
+
+// Задача 5.5 NaN
+let str1 = 'This is a first string';
+let str2 = 'This is a second string';
+alert(str1 * str2);
+
+// Задача 5.6 Infinity
+alert(10 / 0);
+
+// Задача 5.7 -Infinity
+alert(-10 / 0);
