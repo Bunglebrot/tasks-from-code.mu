086722c0c08f14d90ec9b1bfcf3d3b99b1c2e573
Nick Khitrov <th1sistom@yandex.ru>
2021-02-01 13:10:48 +0300

    Add a script of solutions for tasks of Strings

---
+++ 0 Основной учебник JavaScript/00 Переменные и операции/4 Строки/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Задача 4.1
+let str = '!!!';
+alert(str);
+
+// Задача 4.2
+let firstName = 'java';
+let secondName = 'script';
+alert(firstName + secondName);
+
+// Задача 4.3
+let greet = 'hello';
+let whoIsGreet = 'world';
+alert(greet + ' ' + whoIsGreet);
+
+// Задача 4.4
+let name = 'Nick';
+alert(greet + ', ' + name);
+
+// Задача 4.5
+let age = 33;
+alert('мне ' + age + ' года');
+
+// Задача 4.6 Длина строки
+let fishText = 'lorem ipsum dollor etc...';
+alert(fishText.length);
+
+// Задача 4.7 Косые кавычки / Многострочность
+str = `a
+b
+c`;
+alert(str);
