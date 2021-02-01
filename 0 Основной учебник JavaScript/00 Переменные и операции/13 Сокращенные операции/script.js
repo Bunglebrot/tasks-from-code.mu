1958eed67d06e44691aabe71c038c478179a2d33
Nick Khitrov <th1sistom@yandex.ru>
2021-02-01 22:15:32 +0300

    Add a script of solutions for tasks of Abbreviated Operations

---
+++ 0 Основной учебник JavaScript/00 Переменные и операции/13 Сокращенные операции/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Задача 13.3 Переписать код с использованием сокращенных операций
+let num = 47;
+num += 7;
+console.log(num);
+num -= 18;
+console.log(num);
+num *= 10;
+console.log(num);
+num /= 15;
+console.log(num);
