c00f6a79fedb14529198606c82b4e049026e967e
Nick Khitrov <th1sistom@yandex.ru>
2021-02-02 21:51:03 +0300

    Add a script of solutions of tasks of the Keys of the Variables

---
+++ 0 Основной учебник JavaScript/01 Коллекции/18 Ключи из переменных/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Задача 18.1
+let arr = ['a', 'b', 'c'];
+let key = 2;
+console.log(arr[key]);
+
+// Задача 18.2
+let arr1 = [1, 2, 3, 4, 5];
+let key1 = 1;
+let key2 = 2;
+let sum1 = arr1[key1] + arr1[key2];
+console.log(sum1);
+
+// Задача 18.3
+let obj = {
+  'a': 1,
+  'b': 2,
+  'c': 3,
+};
+key = 'b';
+console.log(obj[key]);
