a141bb3f43db62aaa8bac7fd46c6874ccd58c824
Nick Khitrov <th1sistom@yandex.ru>
2021-02-03 11:17:07 +0300

    Add a script of solutions for tasks of the 'if-else' Construction

---
+++ 0 Основной учебник JavaScript/02 Условия/21 Конструкция if-else/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Задача 21.1
+let test = 11;
+if (test > 10) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задача 21.2
+if (test < 10) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задача 21.3
+if (test >= 10) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задача 21.4
+if (test <= 10) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задача 21.5
+if (test == 10) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задача 21.6
+if (test != 10) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задача 21.7
+let test1 = 17;
+let test2 = 2;
+if (test1 > test2) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
