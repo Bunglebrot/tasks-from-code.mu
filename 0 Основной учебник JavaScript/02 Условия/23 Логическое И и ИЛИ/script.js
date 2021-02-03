19428cf09825c14cc00d9b1668eb953c53d1e418
Nick Khitrov <th1sistom@yandex.ru>
2021-02-03 12:20:40 +0300

    Add a script of solutions for tasks of Logical 'And' and 'Or'

---
+++ 0 Основной учебник JavaScript/02 Условия/23 Логическое И и ИЛИ/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Задача 23.1
+let num = 3;
+if (num > 0 && num < 5) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задача 23.2
+num = 20;
+if (num >= 10 && num <= 20) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задача 23.3
+let num1 = 1;
+let num2 = 2;
+if (num1 <= 1 && num2 >= 3) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
