d247f2621381c229abb5b1042492ba71afea8702
Nick Khitrov <th1sistom@yandex.ru>
2021-02-04 14:02:10 +0300

    Add a script of sulutions for tasks of 'if-else' Construction and Bollean values

---
+++ 0 Основной учебник JavaScript/02 Условия/25 Конструкция if-else и булевы значения/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Задача 25.1
+let test = true;
+if (test === true) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задача 25.2
+if (test === false) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задачи 25.12 и 25.15
+if (test) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+
+// Задачи 25.13 и 25.14
+if (!test) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
