20a0149cf5d73dd6e44df8944d98eaca017cf7a1
Nick Khitrov <th1sistom@yandex.ru>
2021-02-04 14:20:48 +0300

    Add a script of solutions for tasks of the Abbreviated 'if-else' Syntax

---
+++ 0 Основной учебник JavaScript/02 Условия/26 Сокращенный синтаксис if-else в JavaScript/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Задача 26.1
+console.log('Задача 26.1');
+let test = 6;
+if (test == 10) {
+  console.log('верно');
+}
+
+// Задача 26.2
+if (!test) alert('верно');
+else alert('неверно');
+
+// Задача 26.3
+if (!test) alert('верно');
