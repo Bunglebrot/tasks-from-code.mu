e703b6c6e1334860fbc8311b16397565e9e8bb0d
Nick Khitrov <deliriumbred@yandex.ru>
2021-02-09 12:42:09 +0300

    Add a script of a solution for a task of the 'Switch Case' construction

---
+++ 0 Основной учебник JavaScript/02 Условия/31 Конструкция switch-case/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Задача 31.1
+let num = 3;
+switch(num) {
+  case 1 :
+    console.log('зима');
+  break;
+  case 2 :
+    console.log('весна');
+  break;
+  case 3 :
+    console.log('лето');
+  break;
+  case 4 :
+    console.log('осень');
+  break;
+  default:
+    console.log('число > 4 или < 1');
+  break;
+}
