190c70749c9cd9ec663d77cc627586000a2a48f7
Nick Khitrov <deliriumbred@yandex.ru>
2021-02-09 12:59:37 +0300

    Add a script of solutions for tasks of Logical operations

---
+++ 0 Основной учебник JavaScript/02 Условия/33 Логические операции/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Задача 33.1
+let a = 2 * (3 - 1);
+let b = 6 - 2;
+let res1 = a == b;
+console.log(res1);
+
+// Задача 33.2
+let a2 = 5 * (7 - 4);
+let b2 = 1 + 2 + 7;
+let res2 = a2 > b2;
+console.log(res2);
+
+// Задача 33.3
+let a3 = 2 ** 4;
+let b3 = 4 ** 2;
+let res3 = a3 != b3;
+console.log(res3);
