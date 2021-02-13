bb009a30963c515c7c7f4402f18dbd868344bdc2
Nick Khitrov <deliriumbred@yandex.ru>
2021-02-13 11:17:04 +0300

    Add scripts of solutions for tasks of the While loop

---
+++ 0 Основной учебник JavaScript/03 Циклы/35 Цикл while/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// Задача 35.1
+let i = 1;
+while (i <= 100) {
+  console.log(i);
+  i++;
+}
+
+// Задача 35.2
+console.log('Задача 35.2');
+let j = 11;
+while (j <= 33) {
+  console.log(j);
+  j++;
+}
+
+// Задача 35.3
+console.log('Задача 35.3');
+let k = 0;
+while (k < 100) {
+  k += 2;
+  console.log(k);
+}
+
+// Задача 35.4
+console.log('Задача 35.4');
+let l = 1;
+while (l < 100) {  
+  console.log(l);
+  l += 2;
+}
+
+// Задача 35.5
+console.log('Задача 35.5');
+let m = 30;
+while (m >= 0) {
+  console.log(m);
+  m--;
+}
